91f1134eda5b44f577a64575a3b91cf9f1f974c7
Carolina05p <107445794+Carolina05p@users.noreply.github.com>
2022-09-14 23:33:32 -0400

    Added dot plot

---
+++ Python_if.py
@@ -0,0 +1,16 @@
+
+
+num1 = 12
+key = False
+
+if num1 == 12:
+    if key:
+        print('Num1 is equal to Twelve and they have the key!')
+    else:
+        print('Num1 is equal to Twelve and they Do NOT have the key!')
+elif num1 < 12:
+    print('Num1 is less than Twelve!')
+else:
+    print('Num1 is NOT equal to Twelve!')
+
+
